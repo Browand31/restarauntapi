c9c74f673000a20fb15a53c6ea70a0dd385b76f7
Browand31 <90056711+Browand31@users.noreply.github.com>
2021-12-17 05:12:46 +1300

    Add files via upload

---
+++ restaraunts api/script.js
@@ -0,0 +1,8 @@
+const site  = "https://iskarr.github.io/austindonovan.github.io/api/business.json";
+
+fetch(https,//iskarr.github.io/austindonovan.github.io/api/business.json)
+
+fetch.then((response) => response.json())
+.then((jsObject)) , {
+    console,log(jsObject)
+})
